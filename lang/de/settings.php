0f5ce3c5fc287487949741b1a4ad1515ecb24fed
Dominik <dominik@reichardt-online.it>
2016-02-21 12:41:07 +0100

    Create settings.php

---
+++ lang/de/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ * @author     Dominik Reichardt <dominik@reichardt-online.it>
+ */
+// for the configuration manager
+$lang['useMetaDescription']   = 'Meta-Tag Beschreibung hinzu';
+$lang['useMetaAuthor']        = 'Meta-Tag Autor hinzufügen';
+$lang['useDublinCore']        = 'Füge Dublin Core Meta-Daten hinzu';
+$lang['useJSONLD']            = 'JSON-LD hinzufügen';
+$lang['defaultSchemaOrgType'] = 'Standard Schema.org Typ für JSON-LD';
+$lang['excludedPages']        = 'Ausgeschlossene Seiten (benutze Reguläre Ausdrücke)';
